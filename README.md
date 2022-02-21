e139465c81b8e427685ab700bf92441cb5cc4137
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-02-21 22:40:26 +0100

    built a CsvToJson app

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # CSVtoJSON
+CsvtoJson converts Csv files to json file asynchronously
+
+To Start the server hit "npm start" in the terminal
+
+To generate the json file hit the endpoint "http://localhost:5000/convert" on a browser
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,35 @@
 import  express, { NextFunction, Request, Response} from "express";
+import csvtojson from "csvtojson";
+import * as fs from 'fs';
+import { json } from "body-parser";
 
 const app = express();
 const port:number = 5000;
+const csvFile = "sample.csv"
 
-app.get('/',  (req:Request, res:Response)=>{
+app.get('/convert',  (req:Request, res:Response)=>{
     res.send(`We are live`)
+
+    //Convert csv to json
+    csvtojson()
+    .fromFile(csvFile)
+    .then((json)=>{
+        console.log(json);
+
+        // map through to check for missing data
+         json.map((item)=>{
+            if(item.phone === ""){
+                item.phone = 'Missing Data'
+            }
+        })
+        
+        // Store file output in a json file
+         fs.writeFile('file.json', JSON.stringify(json), "utf-8", (err) =>{
+            if (err) console.log(err);
+            
+        })
+    })
+
 })
 
 app.listen(port,  ()=>{
